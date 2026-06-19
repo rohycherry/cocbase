b5de25d12e10291faefed1a70512dea9a33bf988
Mizanur Rahaman <131800259+rohycherry@users.noreply.github.com>
2026-06-20 04:05:36 +0800

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -11,6 +11,7 @@ source = response.json()
 matches = []
 
 source["matches"].sort(key=lambda x: (x["date"], x.get("time", "")))
+
 for m in source["matches"]:
 
     stage = m.get("group", m.get("round", "World Cup"))
